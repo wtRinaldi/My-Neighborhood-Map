7c7f44cfd7a337af13572e06b8a39c6744360680
William Rinaldi <williamtrinaldi@gmail.com>
2016-01-20 10:21:03 -0700

    added foursquare ajax request

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+var request = {
+		location: {lat:33.6038895, lng:-111.7262195},
+		radius: 50,
+		query: 'food'
+},
+	fourSqClientID = 'B3GBLGC2TFDYJCL4HLM44XYRQ5MG1HF1PFBZUWOL0JQNLBL3',
+	fourSqClientSecret = 'ODHJOFM0ABVVHB5AHXHZGPMP3QWOAAWDN4D1RIOL3DEKUVEW'
+
+
 
 var myCenter = new google.maps.LatLng(33.6038895,-111.7262195);
 
@@ -21,11 +30,33 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
+var fourSquareUrl = "https://api.foursquare.com/v2/venues/search"+
+  "?client_id="+ fourSqClientID +
+  "&client_secret="+ fourSqClientSecret +
+  "&v=20160115"+
+  "&ll=40.7,-74"+
+  "&query=sushi";
 
+
+	$.getJSON( "fourSquareUrl", function( data ) {
+  		$.each(data.response.venues, function (i, venues) {
+        	content = '<p>Name: ' + venues.name +
+            ' Address: ' + venues.location.address +
+            ' Lat/long: ' + venues.location.lat + ', ' + venues.location.lng + '</p>';
+    	$(content).appendTo("#venues");
+    });
+
+});
+
+
+
+
+/*
 function RestaurantProfile(name) {
 	var self = this;
 	self.name = name;
 }
+
 
 
 function appViewModel() {
@@ -41,7 +72,8 @@ function appViewModel() {
 
 };
 
-ko.applyBindings(new appViewModel());
+ko.applyBindings(new appViewModel()); 
+*/
 
 
 
