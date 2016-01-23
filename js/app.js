8e49e07dbafe178bc8504f703b17f5882db21173
William Rinaldi <williamtrinaldi@gmail.com>
2016-01-23 13:53:09 -0700

    Implemented list filter & some CSS changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
-function sushiRestaurant(name) {
+function sushiRestaurant(name, lat, lng) {
 	var self = this;	
-	self.name = name;
-	//self.location = location;
+	self.name = ko.observable(name);
+	self.lat = lat;
+	self.lng = lng;
 }
 
 function appViewModel(){
 
 	var self = this;
-
-	self.localRestaurant = ko.observableArray([
-		
-	]);
-	
 
 	var fourSqClientID = 'B3GBLGC2TFDYJCL4HLM44XYRQ5MG1HF1PFBZUWOL0JQNLBL3';
 	var fourSqClientSecret = 'ODHJOFM0ABVVHB5AHXHZGPMP3QWOAAWDN4D1RIOL3DEKUVEW';
@@ -20,22 +16,34 @@ function appViewModel(){
 		'&client_secret='+ fourSqClientSecret +
 		'&v=20160115'+
 		'&near='+ searchTown +
-		'&radius=20000'+
+		'&radius=15000'+
 		'&query=sushi';
 
-	$.getJSON(fourSquareUrl, function( data ) {
-		
-		var venuesLength = data.response.venues.length;
-
-		
-		//self.localRestaurant.push(new sushiRestaurant());		
+	$.getJSON(fourSquareUrl, function( data ) {		
+		var venuesLength = data.response.venues.length;		
 		var name;
+		var lat;
+		var lng;
 		for (var i = 0; i < venuesLength; i++) {
 			name = data.response.venues[i].name;
-			self.localRestaurant.push(new sushiRestaurant(name));
-		}
-	  		
-	  	
+			lat = data.response.venues[i].location.lat;
+			lng = data.response.venues[i].location.lng;
+			self.localRestaurant.push(new sushiRestaurant(name, lat, lng));
+		}	  	
+	});
+
+	self.localRestaurant = ko.observableArray([]);
+	
+	self.filter = ko.observable('');
+	filteredRestaurant = ko.computed(function() {
+       	var filter = self.filter().toLowerCase();
+    		if (!filter) {
+        		return self.localRestaurant();
+    		} else {
+        		return ko.utils.arrayFilter(self.localRestaurant(), function(sushiRestaurant) {
+        			return !sushiRestaurant.name().toLowerCase().indexOf(filter);
+        		});
+    		}
 	});
 
 	var myCenter = new google.maps.LatLng(33.6038895,-111.7262195);
@@ -47,7 +55,7 @@ function appViewModel(){
     	mapTypeId:google.maps.MapTypeId.ROADMAP
   	};
 
-  	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
   	var marker = new google.maps.Marker({
 		position:myCenter,
