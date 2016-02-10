a590f6c9cc6aa0643610af7efbcea9b2deb99675
William Rinaldi <williamtrinaldi@gmail.com>
2016-02-10 12:49:17 -0700

    only one infoWindow at a time

--- js/app.js
+++ js/app.js
@@ -28,46 +28,47 @@ var appViewModel = function() {
 			lng = data.response.venues[i].location.lng;
 			address = data.response.venues[i].location.address;
 			phone = data.response.venues[i].contact.formattedPhone;
-		if (address && phone) {
-			locationData.push({locationName: name, latLng: {lat: lat, lng: lng}, locationAddress: address, locationPhone: phone});
-		}
-		}
+			if (address && phone) {
+				locationData.push({locationName: name, latLng: {lat: lat, lng: lng}, locationAddress: address, locationPhone: phone});
+			};
+		};
 		//run function to populate array after the data from JSON request is received
-		getData();
-	});
+		setData();
+	})
+	.error(function() { alert("Sorry, no sushi for you.  The servers are down.");});
 // creates new google map
 	var googleMap = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 33.5, lng: -111.9333},
 		zoom: 11
 	});
+// new infowindow
+	var InfoWindow = new google.maps.InfoWindow();
 // stores all place objects
 	self.allPlaces = [];
 // array stores all filtered locations 
 	self.visiblePlaces = ko.observableArray([]);
-
 // this is the user input for the filter
 	self.userInput = ko.observable('');
-
 // process to get data and run infoWindow
-	function getData() {
-// pushes instances of place into array
-		locationData.forEach(function(place) {
-			self.allPlaces.push(new Place(place));
-		});
+	function setData() {
 //function to create an instance of Place
-		function Place(data) {
+		self.Place = function (data) {
 			this.locationName = data.locationName;
 			this.latLng = data.latLng;
 			this.address = data.locationAddress;
 			this.phone = data.locationPhone;
 			this.marker = null;
+			this.InfoWindow = null;
 			//allows for ko.to open window with button click
 			this.selectLocation = function() {
 				this.marker.InfoWindow.open(googleMap);
 				this.marker.setAnimation(google.maps.Animation.BOUNCE);
 			};
 		};
-
+		// pushes instances of place into array
+		locationData.forEach(function(place) {
+			self.allPlaces.push(new self.Place(place));
+		});
 // for each place in array sets location and options for marker
 		self.allPlaces.forEach(function(place) {
 			var markerOptions = {
@@ -75,65 +76,54 @@ var appViewModel = function() {
 				position: place.latLng,
 				draggable: false,
 				animation: google.maps.Animation.DROP
-			}
-// creates a new marker with options
-		place.marker = new google.maps.Marker(markerOptions);
-// creates a infoWindow for marker
-		place.marker.InfoWindow = new google.maps.InfoWindow({
-			position: place.latLng,
-			pixelOffset: new google.maps.Size(20, -20),
-			content: createContent(place.locationName, place.phone, place.address)
-		});
-// listener for marker - opens infoWindow and toggles bounce
-		place.marker.addListener('click', function () {
-			toggleBounce();
-			openInfoWindow();
-		});
-// turns off animation when window is closed
-		place.marker.InfoWindow.addListener('closeclick', function () {
-			place.marker.setAnimation(null);
-		});
-// toggles bounce function
-		function toggleBounce() {
-			if (place.marker.getAnimation() !== null) {
-				place.marker.setAnimation(null);
-			} else {
-				place.marker.setAnimation(google.maps.Animation.BOUNCE);
 			};
-		};
-// function to open infoWindow
-		function openInfoWindow() {
-			place.marker.InfoWindow.open(googleMap);
-		};
-	});
-	
+			var InfoWindowOptions = {
+				position: place.latLng,
+				pixelOffset: new google.maps.Size(20, -20),
+				content: createContent(place.locationName, place.phone, place.address)
+			};
+// creates a new marker with options
+			place.marker = new google.maps.Marker(markerOptions);
+// listener for marker - opens infoWindow and toggles bounce
+			place.marker.addListener('click', function () {
+				InfoWindow.setOptions(InfoWindowOptions);
+				InfoWindow.open(googleMap, place.marker);
+				toggleBounce();
+			});
+
+// toggles bounce function
+			function toggleBounce() {
+				if (place.marker.getAnimation() !== null) {
+					place.marker.setAnimation(null);
+				} else {
+					place.marker.setAnimation(google.maps.Animation.BOUNCE);
+				};
+			};
+		});
 // takes each place in the allPlaces array and pushes them into the KO observable array
-	self.allPlaces.forEach(function(place) {
-		self.visiblePlaces.push(place);
-	});
-};
-
-
-// filter looks to the KO observable array and checks for matching conditions of the user input into filter bar
-self.filterMarkers = function() {
-	// sets all inputs to lowercase 
-	var searchInput = self.userInput().toLowerCase();
-// removes all visible places
-	self.visiblePlaces.removeAll();
-// looks over each place in allPlaces array 
-	self.allPlaces.forEach(function(place) {
-// sets are markers to not visible
-		place.marker.setVisible(false);
-// anything at is at the index of the search should be pushed to the visible array
-		if (place.locationName.toLowerCase().indexOf(searchInput) !== -1) {
+		self.allPlaces.forEach(function(place) {
 			self.visiblePlaces.push(place);
-		}
-	});
-// sets all markers to visible in the visiblePlaces KO observable array
-	self.visiblePlaces().forEach(function(place) {
-	  place.marker.setVisible(true);
-	});
+		});
+	};
+// filter looks to the KO observable array and checks for matching conditions of the user input into filter bar
+	self.filterMarkers = function() {
+	// sets all inputs to lowercase 
+		var searchInput = self.userInput().toLowerCase();
+	// removes all visible places
+		self.visiblePlaces.removeAll();
+	// looks over each place in allPlaces array 
+		self.allPlaces.forEach(function(place) {
+	// sets are markers to not visible
+			place.marker.setVisible(false);
+	// anything at is at the index of the search should be pushed to the visible array
+			if (self.place.locationName.toLowerCase().indexOf(searchInput) !== -1) {
+				self.visiblePlaces.push(place);
+			}
+		});
+	// sets all markers to visible in the visiblePlaces KO observable array
+		self.visiblePlaces().forEach(function(place) {
+		  place.marker.setVisible(true);
+		});
+	};
 };
-};
-
 ko.applyBindings(new appViewModel());
